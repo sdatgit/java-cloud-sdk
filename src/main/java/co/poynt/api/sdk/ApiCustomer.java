ba6eb04c0e4fdf151e676b88d55d7f21e4833e58
Sanjay Dalal <sanjay@wavelety.com>
2021-01-13 23:03:10 -0800

    SOL-385 uses next link instead of creating url

--- src/main/java/co/poynt/api/sdk/ApiCustomer.java
+++ src/main/java/co/poynt/api/sdk/ApiCustomer.java
@@ -16,35 +16,27 @@ import co.poynt.api.model.CustomerList;
 
 //by Wavelety, Inc.
 public class ApiCustomer extends Api {
-    public static final String API_CUSTOMERS = "/businesses/{businessId}/customers?limit=100&startOffset={startOffset}";//&startAt={startAt}&endAt={endAt}";
+    public static final String API_CUSTOMERS = "/businesses/{businessId}/customers?limit=100";
     private static final Logger logger = LoggerFactory.getLogger(ApiCustomer.class);
 
     public ApiCustomer(PoyntSdk sdk) {
         super(sdk, Constants.POYNT_API_HOST + API_CUSTOMERS);
     }
 
-    public List<Customer> getAll(String businessId) {
-
-        return getAllFromBusiness(Customer.class, businessId);
-    }
 
     public Customer get(String businessId, String customerId) {
         return this.getFromBusiness(Customer.class, businessId, customerId);
     }
 
-    public List<Customer> getAllCustomers(String businessId) {
-        CustomerList list = (CustomerList) getAllFromBusiness(CustomerList.class, businessId);
-        return list.getCustomers();
-    }
 
-    public List<Customer> getAll(String businessId, String startAt, String endAt, int offset) {
+    public CustomerList getAll(String businessId, String next) {
         CustomerList result = null;
         String accessToken = sdk.getAccessToken();
 
         String baseUrl = this.endPoint.replace("{businessId}", businessId);
-//        baseUrl = baseUrl.replace("{startAt}", startAt);
-//        baseUrl = baseUrl.replace("{endAt}", endAt);
-        baseUrl = baseUrl.replace("{startOffset}", "" + offset);
+        if(next != null) {
+            baseUrl = Constants.POYNT_API_HOST + next;
+        }
         
         logger.info("getAll: url={}", baseUrl);
         HttpGet get = this.createGetRequest(baseUrl);
@@ -65,10 +57,7 @@ public class ApiCustomer extends Api {
         } finally {
             get.releaseConnection();
         }
-        if (result != null && result.getCount() > 0) {
-            return result.getCustomers();
-        }
-        return new ArrayList<Customer>();
+        return result;
 
     }
 }
